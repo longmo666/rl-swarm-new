9b62de876ee196fbbeb31eefcc934696d08d1ef0
beryll1um <mr.anarchist1934@gmail.com>
2025-05-01 22:12:30 +0200

    Fix DHT failed to start in 30.0 seconds (#159)

--- hivemind_exp/runner/grpo_runner.py
+++ hivemind_exp/runner/grpo_runner.py
@@ -17,6 +17,7 @@ try:
 except ImportError:
     pass
 
+import time
 import hivemind
 from datasets import Dataset
 from huggingface_hub import login
@@ -43,6 +44,7 @@ class GRPOArguments:
     public_maddr: str | None = None
     host_maddr: str | None = None
     identity_path: str | None = None
+    dht_retry_timeout: float = 60.0
     max_rounds: int = 100
 
     # Model arguments
@@ -139,7 +141,23 @@ class GRPORunner:
 
     def setup_dht(self, grpo_args):
         initial_peers = grpo_args.initial_peers
-        dht = hivemind.DHT(start=True, startup_timeout=30, **self._dht_kwargs(grpo_args))
+        dht_retry_timeout = grpo_args.dht_retry_timeout
+
+        while True:
+            try:
+                dht = hivemind.DHT(start=True, **self._dht_kwargs(grpo_args))
+                # In case everything succeeded and no exception were raised.
+                break
+            except Exception as e:
+                # Its better to log this behavior to simplify investigation
+                # in the future.
+                logger.warning(
+                    f"Failed to start DHT peer due to exception: `{e}`. "
+                    f"Will try it again in {dht_retry_timeout} seconds...")
+                # I'm not sure progressive timeout is required here now, so
+                # let it be static now.
+                time.sleep(dht_retry_timeout)
+
         if initial_peers:
             logger.info(f"🐝 Joining swarm with initial_peers = {initial_peers}")
         else:
